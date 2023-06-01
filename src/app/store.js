57ed0ba087c2ab1a314d7fc2f7479c9ec11d6506
Antonio-Italiano <antoitalia221@gmail.com>
2023-06-01 17:54:46 +0200

    Implemented Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todosReducer from '../features/todos/todosSlice.js';
 import filterReducer from '../features/todos/filterSlice'
+
+const myLog = store => nextMiddleware => action => {
+      // store.dispatch({type:'INIT_MY_LOG', payload: null })
+      console.log(action.payload);
+      // return 'block';
+      return nextMiddleware(action);
+    }
+
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
     todos: todosReducer,
   },
+  
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(myLog),
+  
 });
